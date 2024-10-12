98172407edc847168e6702547ccbcdf47f057559
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-10-12 13:15:32 +0300

    Add files via upload

---
+++ 07. Forms/02. controlled-components-demo/client/src/App.jsx
@@ -0,0 +1,14 @@
+import ControlledForm from './components/ControlledForm'
+
+import './App.css'
+
+function App() {
+
+    return (
+        <>
+            <ControlledForm />
+        </>
+    )
+}
+
+export default App
