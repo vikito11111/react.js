d5c17d968ef03c1689cff4d3501e868c493ce28c
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-12-31 03:51:50 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using _02._students_orm_test_demo.Models;
+
+var dbContext = new StudentDbContext();
+
+dbContext.Database.EnsureCreated();
+
+dbContext.Courses.Add(new Course { Name = "Entity Frakework Core" });
+dbContext.Courses.Add(new Course { Name = "SQL Server" });
+
+dbContext.SaveChanges();
